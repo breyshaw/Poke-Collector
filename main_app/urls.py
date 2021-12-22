582641eeb1832148e3d7e32edfa9c3a3607fdd15
breyshaw <dawsonbrey@gmail.com>
2021-12-21 19:19:47 -0500

    define view functions, create about template and render functionality

---
+++ main_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+  # This defines a root path using an emtpy string and maps it to the view.home function
+  # The name='home' kwarg is optional but will come in handly referencing the URL from other parts of the app.
+  path('', views.home, name='home'),
+  # Trailing slashes are convention for Django
+  path('about/', views.about, name='about')
+]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+#For testing purposes
+from django.http import HttpResponse
 
-# Create your views here.
+# Defining the home view
+def home(request):
+  return HttpResponse('<h1>Gotta Catch Em All!</h1>')
+
+def about(request):
+  return render(request, 'about.html')
+
