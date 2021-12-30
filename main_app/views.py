5c7ab2a29dcc0100a0f20c77fec075a0bd77238c
breyshaw <dawsonbrey@gmail.com>
2021-12-30 13:13:48 -0500

    add pokemon detail functionality

--- main_app/views.py
+++ main_app/views.py
@@ -10,6 +10,12 @@ def pokemon_index(request):
   pokemon = Pokemon.objects.all()
   return render(request, 'pokemon/index.html', {'pokemon': pokemon})
 
+def pokemon_detail(request, pokemon_id):
+  pokemon = Pokemon.objects.get(id=pokemon_id)
+  return render(request, 'pokemon/detail.html', {'pokemon': pokemon})
+
 def about(request):
   return render(request, 'about.html')
+
+
 
